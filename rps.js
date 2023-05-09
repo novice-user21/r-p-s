a6bbbb21c366c56ec6ae5bf093994595af903e5c
novice-user21 <128810672+novice-user21@users.noreply.github.com>
2023-05-09 11:36:21 +0530

    Add computer choice

--- rps.js
+++ rps.js
@@ -1,1 +1,25 @@
-console.log("yoi");
+let usrChoice = prompt("choos any of rock paper scissor").toLowerCase(); 
+
+let  computeeSelection = function getComputerChoice() {
+    switch (Math.floor(Math.random()*10))
+    {   
+        case 9:
+        case 0:
+        case 3:
+        case 6:
+        return "rock";
+        break;
+        case 1:
+        case 4:
+        case 7:
+        return "paper";
+        break;
+        case 2:
+        case 5:
+        case 8:
+        return "scissor";
+    }
+   
+} 
+
+console.log(getComputerChoice());a
